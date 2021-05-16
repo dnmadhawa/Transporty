37f0db6117ea8b73df43ecc6b8acb3f72bc147ca
iresha1999 <it20647582@my.sliit.lk>
2021-05-16 23:52:56 +0530

    end

---
+++ js/transporty.js
@@ -0,0 +1,36 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+var myTimer = setInterval(next, 4000,1);
+
+function next(n) {
+  clearInterval(myTimer);
+  showSlides(slideIndex += n);
+}
+
+function previous(n) {
+  clearInterval(myTimer);
+  showSlides(slideIndex += n);
+}
+
+function showSlides(a) {
+  var x = document.getElementsByClassName("slide");
+  if (a > x.length){
+    slideIndex = 1;
+  }
+
+  if (a < 1) {
+    slideIndex = x.length;
+  }
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+
+  x[slideIndex-1].style.display = "block";  
+  a =slideIndex+1;
+  myTimer = setInterval(next, 10000,1);
+
+}
+
+
+
