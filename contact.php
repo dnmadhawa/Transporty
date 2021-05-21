8d4b07add97ccaa67582b29e736cacae8c568cb1
iresha1999 <it20647582@my.sliit.lk>
2021-05-21 13:43:51 +0530

    contact done

---
+++ contact.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contac us</title>
+	<link rel="stylesheet" type="text/css" href="css/contact.css">
+	
+</head>
+<body>
+	<dv class="top-nav-bar container"><!-- top-nav-bar -->
+	<div class="logo-d"><!-- logo-d -->
+		<a class="logo" href="#"><img src="img/logo.png"></a>
+	</div><!-- /logo-d -->
+	<div class="login"><!-- login -->
+		<input type="button" name="login" value="Login">
+		<img src="img/user.png">
+	</div><!-- /login -->
+    </dv><!-- /top-nav-bar -->
+
+    <div class="nav-bar container"><!-- nav-bar -->
+	<ul class="manu"><!-- manu -->
+		<li><a href="#"><b>Home</b></a></li>
+		<li><a href="#"><b>All Schedule</b></a></li>
+		<li><a href="#"><b>Service type</b></a></li>
+		<li><a href="#">Contact</a></li>
+		<li><a href="#"><b>About Us</b></a></li>
+	</ul><!-- /manu -->
+	<div class="search-box"><!-- search-box -->
+		<a href=""><img src="img/search.png"></a>
+		<input type="input" name="search" placeholder="Search">
+	</div><!-- /search-box -->
+</div><!-- /nav-bar -->
+
+<div class="slider container" align="center"><!-- slider -->
+  	<img class="slide" src="img/banner1.jpg">
+  	<img class="slide" src="img/banner2.jpg">
+  	<img class="slide" src="img/banner2.jpg">
+
+  	<button class="slider-btn previous-btn" onclick="previous(-1)"><img src="img/previous.png"></button>
+  	<button class="slider-btn next-btn" onclick="next(1)"><img src="img/next.png"></button>
+</div><!-- /slider -->
+
+<br>
+<div class="reach"><!--reach-->
+	<center>
+	<h2>Reach Our Team</h2>
+	<p>We love questions and feedback_and we are always happy to help!<br>Here are some ways to contact us</p>
+
+</center>
+</div><!--reach-->
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="talk"><!--talk-->
+	<h4>Talk With Us</h4>
+	<p>Chat with our team to solve your problem.</p>
+
+	<img src="img/phone22.png">
+    <p1>0112564260</p1>
+
+
+</div><!--talk-->
+
+<div class="customer_support"><!--customer_support-->
+	<h4>Contact Customer Support</h4>
+	<p>We are waiting to help you and your team</p>
+	<button onclick="myFunction()">Contact Support</button>
+
+
+</div><!--customer_support-->
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="head"><!--head-->
+	<h3>Headquarters</h3>
+	<br>
+	<br>
+	<br>
+	<br>
+
+	<div class="information"><!--information-->
+
+		<div class="gmail"><!--gmail-->
+
+           <img src="img/gmail.png">
+           <p>transporty@gmail.com</p>
+
+		</div><!--gmail-->
+
+		<div class="number"><!--number-->
+			<img src="img/telephone.png">
+			<p>0112564260</p>
+
+        </div><!--number-->
+
+        <div class="location"><!--location-->
+        	<img src="img/pin.png">
+        	<p>Colombo 7 st.</p>
+
+        </div><!--location-->
+
+        <div class="time"><!--time-->
+        	<img src="img/wall-clock.png">
+        	<p>24 hours</p>
+
+        </div><!--time-->
+
+
+
+
+	</div><!--information-->
+	<br>
+	<br>
+	<br>
+	<br>
+
+
+	<div class="map"><!--map-->
+		
+		<img src="img/map.png">
+
+		<div class="location1"><!--location-->
+			<img src="img/placeholder (1).png">
+
+		</div><!--location-->
+
+
+	</div><!--map-->
+	
+
+
+
+</div><!--head-->
+
+
+
+
+
+
+  
+
+
+
+
+
+<script type="text/javascript" src="js/contact.js"></script>
+</body>
+</html>
