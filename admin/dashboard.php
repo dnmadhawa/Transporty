6b675b276ca70cfc2d5f996ba7cd0d016368dc45
Nilupul Madhawa <68707780+dnmadhawa@users.noreply.github.com>
2021-05-29 12:51:10 +0530

    dash

---
+++ admin/dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Transporty</title>
+	<link rel="shortcut icon" href="img/logo-icon.png">
+</head>
+<body>
+
+<div class="header">
+	
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,20 +10,6 @@
 
 <?php include 'header.php';?>
 
-<div class="nav-bar container"><!-- nav-bar -->
-	<ul class="manu"><!-- manu -->
-		<li><a href="#">Home</a></li>
-		<li><a href="#">All Schedule</a></li>
-		<li><a href="#">Service type</a></li>
-		<li><a href="#">Contact</a></li>
-		<li><a href="#">About Us</a></li>
-	</ul><!-- /manu -->
-	<div class="search-box"><!-- search-box -->
-		<a href=""><img src="img/search.png"></a>
-		<input type="input" name="search" placeholder="Search">
-	</div><!-- /search-box -->
-</div><!-- /nav-bar -->
-
 <div class="slider container" align="center"><!-- slider -->
   	<img class="slide" src="img/banner/banner1.png">
   	<img class="slide" src="img/banner/banner2.png">
@@ -108,49 +94,7 @@
 	</div>
 </div><!-- /service-type -->
 
-<div class="footer container"><!-- footer -->
-	<div class="f-quick-links">
-		<h2>Quick Links</h2>
-		<ul>
-			<li><a href="#">Home</a></li>
-			<li><a href="#">All Schedule</a></li>
-			<li><a href="#">Service type</a></li>
-			<li><a href="#">Contact</a></li>
-			<li><a href="#">About Us</a></li>
-		</ul>
-	</div>
-	<div class="f-about-us">
-		<h2>About Us</h2>
-		<p>We have been a pioneer in booking online bus tickets in Sri Lanka since 2021.Through this service, long distance travelers can book bus seats anywhere in Sri Lanka. This is open to you 24 hours a day.</p>
-	</div>
-	<div class="f-contact">
-		<h2>Contact Us</h2>
-		<p>transporty(pvt) Ltd,<br>
-			Malabe, <br>
-			Colombo. <br>
-			Tel:0112356365 <br>
-			Email:info@transporty.lk</p>
-	</div>
-	<div class="news">
-		<center>
-		<h2>News Letter</h2>
-		<p>Get notified about the next update</p>
-		<form class="subr-form">
-			<input type="Email" name="news" placeholder="Email">
-			<button type="submit"><img src="img/btn-arrow.svg"></button>
-		</form>
-		<div class="social-icon">
-			<a href="www.faceook.com"><img src="img/social-media/facebook.svg"></a>
-			<a href="www.messenger.com"><img src="img/social-media/messenger.svg"></a>
-			<a href="www.twitter.com"><img src="img/social-media/twitter.svg"></a>
-			<a href="www.whatsapp.com"><img src="img/social-media/whatsapp.svg"></a>
-		</div>
-		</center>
-	</div>
-</div><!-- /footer -->
-<div class="copyrigth">
-	<p>Copyrigth ©2021 transporty.lk - All right reserved</p>
-</div>
+<?php include 'footer.php';?>
 <script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
